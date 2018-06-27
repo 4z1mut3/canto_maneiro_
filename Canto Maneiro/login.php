bb94f311e9f8eec1633b606cce8ab508ba2ba040
williamvictor77 <40376100+williamvictor77@users.noreply.github.com>
2018-06-27 11:44:52 -0300

    Add files via upload
    alterei as configurações de login,criei um novo codigo para login e alterei no armaze.php.

---
+++ Canto Maneiro/login.php
@@ -0,0 +1,22 @@
+<?php 
+  $login = $_POST['logar'];
+  $entrar = $_POST['entrar'];
+  $senha = md5($_POST['senha_log']);
+  $connect = mysqli_connect('localhost','root','','usuarios');
+    if (isset($entrar)) {
+             
+      $verifica = mysqli_query($connect,"SELECT * FROM cadastro WHERE email = '$login' AND senha = '$senha'") or die("erro ao selecionar");
+        if (mysqli_num_rows($verifica)<=0){
+          echo"<script language='javascript' type='text/javascript'>alert('Login e/ou senha incorretos');window.location.href='login.html';</script>";
+          die();
+        }else{
+          setcookie("login",$login);
+          header("Location:armaze.php");
+        }
+    }
+    $login_cookie = $_COOKIE['nome'];
+    if(isset($login_cookie)){
+      echo"Bem-Vindo, $login_cookie <br>";
+      echo"Essas informações <font color='red'>PODEM</font> ser acessadas por você";
+    }
+?>
